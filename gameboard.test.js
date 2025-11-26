59dfed087517966c5a26812fc399d0bb4196121b
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-11-26 20:21:09 +0800

    add some code

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,5 +1,5 @@
-import Gameboard from "./gameboard";
-import Ship from "./battleship";
+import Gameboard from "./JS/gameboard/gameboard";
+import Ship from "./JS/ship/battleship";
 
 test('test board', () => {
     const board = new Gameboard();
@@ -17,10 +17,12 @@ test('test board', () => {
 
 test('add ship', () => {
     const board = new Gameboard();
-    const ship = new Ship(4);
+    const ship = new Ship(4, 'hori');
+    const ship1 = new Ship(4, 'vert');
 
     board.createBoard();
     board.addShip(ship, 1, 1);
+    board.addShip(ship1, 2, 1);
 
     expect(board.board[0][0].haveShip)
     .toBe(true);
@@ -34,19 +36,25 @@ test('add ship', () => {
     expect(board.board[0][3].haveShip)
     .toBe(true);
 
+    expect(board.board[1][0].haveShip).toBe(true);
+
 });
 
 test('attack ship', () => {
     const board = new Gameboard();
-    const ship = new Ship(4);
+    const ship = new Ship(4, 'hori');
 
     board.createBoard();
     board.addShip(ship, 1, 1);
     board.receiveAttack(1,1);
+
+    expect(board.board[0][0].shipHere.position).toBe('hori');
 
     expect(board.board[0][0].shipHere.hits).toBe(1);
 
     expect(board.receiveAttack(1,1)).toBe('Already Attacked!');
 
     expect(board.board[0][0].shipHere.hits).toBe(1);
+    board.receiveAttack(1,2);
+    expect(board.board[0][1].shipHere.hits).toBe(2);
 });
